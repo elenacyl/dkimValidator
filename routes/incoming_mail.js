2ae2ab23a11ad1683323df9fb9b8e75445f5e517
Elena Choo Yue Lin <Elenacyl@Elenas-MacBook-Air.local>
2018-05-18 00:11:58 +0800

    add stuff

---
+++ routes/incoming_mail.js
@@ -0,0 +1,18 @@
+//receiving email
+var express = require('express');
+var app = module.exports = express.createServer()
+  , formidable = require('formidable')
+
+  app.post('/incoming_mail', function(req, res){
+  var form = new formidable.IncomingForm()
+  form.parse(req, function(err, fields, files) {
+    console.log(fields.to)
+    console.log(fields.from)
+    console.log(fields.subject)
+    console.log(fields.message)
+    res.writeHead(200, {'content-type': 'text/plain'})
+    res.end('Message Received. Thanks!\r\n')
+  })
+})
+
+app.listen(3000);
