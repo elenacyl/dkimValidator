bb5ea9cd753636f4e35b285a24640a5e241582d0
Elena Choo Yue Lin <Elenacyl@Elenas-MacBook-Air.local>
2018-05-19 21:50:05 +0800

    test

--- routes/incoming_mail.js
+++ routes/incoming_mail.js
@@ -24,18 +24,18 @@ router.post('/', function(req, res){
     res.end('Message Received. Thanks!\r\n')
 
     //message must be a buffer
-    var fileBuffer = fs.readFile(files, function(err, buffer) {
-      if (err) throw err;
-      console.log(buffer);
-    });
-    dkim.verify(fileBuffer, function(error, res){
+    // var fileBuffer = fs.readFile(files, function(err, buffer) {
+    //   if (err) throw err;
+    //   console.log(buffer);
+    // });
+    // dkim.verify(fileBuffer, function(error, res){
 
-      assert.ifError( error )
-      assert.ok( res && res.length > 0 )
-      assert.ok(res.every( function (record) {
-        return record.verified
-      }))
-      done( error)
+    //   assert.ifError( error )
+    //   assert.ok( res && res.length > 0 )
+    //   assert.ok(res.every( function (record) {
+    //     return record.verified
+    //   }))
+    //   done( error)
 
 
       //mailgun
