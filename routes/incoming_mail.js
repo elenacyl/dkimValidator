9703237a2cb7f7ea3b4de53a467c0c1dacbb1f74
Elena Choo Yue Lin <Elenacyl@Elenas-MacBook-Air.local>
2018-05-19 10:16:32 +0800

    add stuff

--- routes/incoming_mail.js
+++ routes/incoming_mail.js
@@ -5,25 +5,35 @@ var dkim = require('dkim');
 var app = module.exports = express.createServer()
   , formidable = require('formidable')
 
-  app.post('/incoming_mail', function(req, res){
-  var form = new formidable.IncomingForm()
-  form.parse(req, function(err, fields, files) {
-    console.log(fields.to)
-    console.log(fields.from)
-    console.log(fields.subject)
-    console.log(fields.message)
-    res.writeHead(200, {'content-type': 'text/plain'})
-    res.end('Message Received. Thanks!\r\n')
+var app = express();
+app.listen(process.env.PORT || 3000);
 
-    //message must be a buffer
-    var fileBuffer = new Buffer(files);
-    dkim.verify(fileBuffer, function(req, res){
+app.post('/incoming_mail', function(req, res) { 
+  console.log("Got to this point");
+  console.log(req);
+})
+//   app.post('/incoming_mail', function(req, res){
+//   var form = new formidable.IncomingForm()
+//   form.parse(req, function(err, fields, files) {
+//     if (err) {
+//       console.log(err);
+//       res.send("Error occured")
+//     }
+//     console.log(fields.to)
+//     console.log(fields.from)
+//     console.log(fields.subject)
+//     console.log(fields.message)
+//     res.writeHead(200, {'content-type': 'text/plain'})
+//     res.end('Message Received. Thanks!\r\n')
 
-      //mailgun should be here
+//     //message must be a buffer
+//     var fileBuffer = new Buffer(files);
+//     dkim.verify(fileBuffer, function(req, res){
 
-    }
+//       //mailgun should be here
 
-  })
-});
+//     }
 
-app.listen(3000);
+//   })
+// });
+
