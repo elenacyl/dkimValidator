28eae7f6a4da4d997a606e29a196670c1fd25cb9
Elena Choo Yue Lin <Elenacyl@Elenas-MacBook-Air.local>
2018-05-19 10:40:42 +0800

    Hihi

--- routes/incoming_mail.js
+++ routes/incoming_mail.js
@@ -7,32 +7,29 @@ var formidable = require('formidable');
 
 
 router.post('/', function(req, res){
-  console.log("Hit here");
-  res.writeHead(200, {'content-type': 'text/plain'})
-  res.end('Message Received. Thanks!\r\n')
 
-  // var form = new formidable.IncomingForm()
-  // form.parse(req, function(err, fields, files) {
-  //   if (err) {
-  //     console.log(err);
-  //     res.send("Error occured")
-  //   }
-  //   console.log(fields.to)
-  //   console.log(fields.from)
-  //   console.log(fields.subject)
-  //   console.log(fields.message)
-  //   res.writeHead(200, {'content-type': 'text/plain'})
-  //   res.end('Message Received. Thanks!\r\n')
+  var form = new formidable.IncomingForm()
+  form.parse(req, function(err, fields, files) {
+    if (err) {
+      console.log(err);
+      res.send("Error occured")
+    }
+    console.log(fields.to)
+    console.log(fields.from)
+    console.log(fields.subject)
+    console.log(fields.message)
+    res.writeHead(200, {'content-type': 'text/plain'})
+    res.end('Message Received. Thanks!\r\n')
 
-  //   //message must be a buffer
-  //   var fileBuffer = new Buffer(files);
-  //   dkim.verify(fileBuffer, function(req, res){
+    //message must be a buffer
+    var fileBuffer = new Buffer(files);
+    dkim.verify(fileBuffer, function(req, res){
 
-  //     //mailgun should be here
+      //mailgun should be here
 
-  //   })
+    })
 
-  // })
+  })
 });
 
 module.exports = router;
